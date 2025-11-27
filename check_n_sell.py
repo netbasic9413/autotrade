7297279934a73e9f086ce237d4cc616aa112cfd0
soonju Jung <netbasic@naver.com>
2025-11-27 22:40:17 +0900

    당일실현손익상세요청에서 해당 종목의 당일매도손익 구현

--- check_n_sell.py
+++ check_n_sell.py
@@ -4,46 +4,57 @@ from sell_stock import fn_kt10001 as sell_stock
 from tel_send import tel_send
 from get_setting import cached_setting
 from login import fn_au10001 as get_token
+from daily_profit_loss import fn_ka10077 as daily_p_l
 import logfile
 
 
 logger = logfile.setup_log()
 
+
 def chk_n_sell(token=None):
 
-	# 익절 수익율(%) - 목표 수익율에 도달하면 매도
-	TP_RATE = cached_setting('take_profit_rate', 10.0)
-	# 손절 수익율(%) - 손실 한계에 도달하면 매도
-	SL_RATE = cached_setting('stop_loss_rate', -10.0)
+    # 익절 수익율(%) - 목표 수익율에 도달하면 매도
+    TP_RATE = cached_setting("take_profit_rate", 10.0)
+    # 손절 수익율(%) - 손실 한계에 도달하면 매도
+    SL_RATE = cached_setting("stop_loss_rate", -10.0)
 
-	try:
-		my_stocks = get_my_stocks(token=token)
-		if not my_stocks:
-			logger.info("보유 종목이 없습니다.")
-			return True
-			
-		for stock in my_stocks:
+    try:
+        my_stocks = get_my_stocks(token=token)
+        if not my_stocks:
+            logger.info("보유 종목이 없습니다.")
+            return True
 
-			# pl_rt는 문자열이므로 float으로 변환하여 비교해야 함
-			pl_rt = float(stock['pl_rt'])
-			if pl_rt > TP_RATE or pl_rt < SL_RATE:
-				time.sleep(0.5)
-				sell_result = sell_stock(stock['stk_cd'].replace('A', ''), stock['rmnd_qty'], token=token)
-				if sell_result != 0:
-					logger.info("매도 실패")
-					return True
+        for stock in my_stocks:
+            # pl_rt는 문자열이므로 float으로 변환하여 비교해야 함
 
-				result_type = "익절" if pl_rt > TP_RATE else "손절"
-				result_emoji = "🔴" if pl_rt > TP_RATE else "🔵"
-				message = f'{result_emoji} {stock["stk_nm"]} {int(stock["rmnd_qty"])}주 {result_type} 완료 (수익율: {pl_rt}%)'
-				tel_send(message)
-				logger.info(message)
+            pl_rt = float(stock["pl_rt"])
+            if pl_rt > TP_RATE or pl_rt < SL_RATE:
+                time.sleep(0.5)
+                sell_result = sell_stock(
+                    stock["stk_cd"].replace("A", ""), stock["rmnd_qty"], token=token
+                )
+                if sell_result != 0:
+                    logger.info("매도 실패")
+                    return True
 
-		return True  # 성공적으로 실행됨
+                # 당일실현손익상세요청
+                dpl = daily_p_l(stock["stk_cd"].replace("A", ""), token=token)
+                tdy_sel_p = dpl.json()["tdy_sel_pl"]
+                f_tdy_sel_p = float(tdy_sel_p)
 
-	except Exception as e:
-		logger.info(f"오류 발생(chk_n_sell): {e}")
-		return False  # 예외 발생으로 실패
+                result_type = "익절" if pl_rt > TP_RATE else "손절"
+                result_emoji = "🔴" if pl_rt > TP_RATE else "🔵"
+                message = f'{result_emoji} {stock["stk_nm"]} {int(stock["rmnd_qty"])}주 {result_type} 완료 (수익율: {pl_rt}%)'
+                message += f"   당일매도손익: {f_tdy_sel_p:,.0f}원\n"
+                tel_send(message)
+                logger.info(message)
+
+        return True  # 성공적으로 실행됨
+
+    except Exception as e:
+        logger.info(f"오류 발생(chk_n_sell): {e}")
+        return False  # 예외 발생으로 실패
+
 
 if __name__ == "__main__":
-	chk_n_sell(token=get_token())
+    chk_n_sell(token=get_token())
---
+++ daily_profit_loss.py
@@ -0,0 +1,61 @@
+import requests
+import json
+from config import host_url
+from login import fn_au10001 as get_token
+import logfile
+
+
+# 당일실현손익상세요청
+def fn_ka10077(stk_cd, cont_yn="N", next_key="", token=None):
+    endpoint = "/api/dostk/acnt"
+    url = host_url + endpoint
+
+    # 2. header 데이터
+    headers = {
+        "Content-Type": "application/json;charset=UTF-8",  # 컨텐츠타입
+        "authorization": f"Bearer {token}",  # 접근토큰
+        "cont-yn": cont_yn,  # 연속조회여부
+        "next-key": next_key,  # 연속조회키
+        "api-id": "ka10077",  # TR명
+    }
+
+    # 2. 요청 데이터
+    params = {
+        "stk_cd": "005930",  # 종목코드
+    }
+
+    # 3. http POST 요청
+    response = requests.post(url, headers=headers, json=params)
+
+    # 4. 응답 상태 코드와 데이터 출력
+    print("Code:", response.status_code)
+    print(
+        "Header:",
+        json.dumps(
+            {
+                key: response.headers.get(key)
+                for key in ["next-key", "cont-yn", "api-id"]
+            },
+            indent=4,
+            ensure_ascii=False,
+        ),
+    )
+    print(
+        "Body:", json.dumps(response.json(), indent=4, ensure_ascii=False)
+    )  # JSON 응답을 파싱하여 출력
+
+    tdy_rlzt_pl_dtl = response.json()["tdy_rlzt_pl_dtl"]
+
+    return tdy_rlzt_pl_dtl
+
+
+# 실행 구간
+if __name__ == "__main__":
+    # 1. 토큰 설정
+    # MY_ACCESS_TOKEN = "사용자 AccessToken"  # 접근토큰
+
+    # 3. API 실행
+    fn_ka10077("005930", token=get_token())
+
+    # next-key, cont-yn 값이 있을 경우
+    # fn_ka10077(token=MY_ACCESS_TOKEN, data=params, cont_yn='Y', next_key='nextkey..')
